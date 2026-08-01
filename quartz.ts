7e5c7c32cec9cc8878207d9c902d300b56970dd5
DinozavrDi <AntonKoren21@yandex.ru>
2026-08-01 13:41:35 +0300

    1

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,13 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+// Sort order: folders first, then files. Sort folders and files alphabetically
+ExternalPlugin.Explorer({
+  filterFn: (node) => {
+    node.displayName = node.displayName.toUpperCase()
+    return node
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
