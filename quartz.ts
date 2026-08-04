3694f43e6a689a07bacd6e9536d794b1636d5cfb
DinozavrDi <AntonKoren21@yandex.ru>
2026-08-04 18:07:34 +0300

    vault backup: 2026-08-04 18:07:34

--- quartz.ts
+++ quartz.ts
@@ -1,7 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-// Sort order: folders first, then files. Sort folders and files alphabetically
-
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
